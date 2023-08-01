6a8152510341c60e8fb5f51baf0e59a2b249661e
Holly Phung <hollyphung95@gmail.com>
2023-08-01 23:53:51 +0000

    Modified function startsWithJ to return only if it starts with CAPITAL J. Modified function categorizeAcidity's conditional logic for 'acidic' to pH >= 0 instead of pH > 0.

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -17,7 +17,7 @@ function isEven(number) {
 }
 
 function startsWithJ(string) {
-  return string[0].toLowerCase() === 'j';
+  return string[0] === 'J';
 }
 
 function isOldEnoughToDrink(person) {
@@ -35,7 +35,7 @@ function isOldEnoughToDrinkAndDrive(person) {
 function categorizeAcidity(pH) {
   if (pH === 7) {
     return 'neutral';
-  } else if (pH > 0 && pH < 7) {
+  } else if (pH >= 0 && pH < 7) {
     return 'acidic';
   } else if (pH > 7 && pH <= 14) {
     return 'base';
